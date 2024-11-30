15fdcaf1d7d8aa4f2dc80cc4cf2647b649a83aad
zoeqnorton <zonorton724@gmail.com>
2024-11-30 15:24:36 -0500

    add function for hamburger icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hello! Welcome to my Portfolio Site")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
