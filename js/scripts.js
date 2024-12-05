191d39aaef6bcb87be8764916d8657df6acb49bd
zoeqnorton <zonorton724@gmail.com>
2024-12-05 09:15:40 -0500

    update scirpts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 console.log("Hello! Welcome to my Portfolio Site")
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
-  }
+    
+    x.className += ' responsive';
+
+}
